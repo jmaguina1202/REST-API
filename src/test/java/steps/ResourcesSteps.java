9fa3e03a815e600a72778f480c155bb2db22a929
Jorge Maguina <je.maguina@gmail.com>
2016-01-20 10:24:04 -0400

    Changed the way the READER was used. Now it uses "get" methods to get the values from the properties file.

--- src/test/java/steps/ResourcesSteps.java
+++ src/test/java/steps/ResourcesSteps.java
@@ -10,36 +10,36 @@ import java.util.Map;
 public class ResourcesSteps {
     private String jsonResponse;
     private CommonUtils util = new CommonUtils();
-    PropertiesReader reader = PropertiesReader.instance();
+    private static final PropertiesReader READER = PropertiesReader.instance();
 
     public String getJsonResponse() {
         return jsonResponse;
     }
 
     @When("^I did a post request:$")
-    public void iDidPostRequest(Map<String, String> data){
+    public void iDidPostRequest(Map<String, String> data)throws Throwable {
         JSONObject project = new JSONObject(data);
-        jsonResponse = util.postRequest(reader.url, reader.authorization, project.toString());
+        jsonResponse = util.postRequest(READER.getEndPoint(), READER.getAuthorizationString(), project.toString());
     }
 
     @When("^I did a get all request")
-    public void iDidGetAllRequest(){
-        jsonResponse = util.getRequest(reader.url, reader.authorization);
+    public void iDidGetAllRequest() throws Throwable {
+        jsonResponse = util.getRequest(READER.getEndPoint(), READER.getAuthorizationString());
     }
 
     @When("^I did a get request with the id \"(.*?)\"")
-    public void iDidGetRequest(String id){
-        jsonResponse = util.getRequestId(reader.url, reader.authorization, id);
+    public void iDidGetRequest(String id) throws Throwable{
+        jsonResponse = util.getRequestId(READER.getEndPoint(), READER.getAuthorizationString(), id);
     }
 
     @When("^I did a put request with the id \"(.*?)\" and the values:$")
-    public void iDidPutRequest(String id,Map<String, String> data){
+    public void iDidPutRequest(String id,Map<String, String> data) throws Throwable {
         JSONObject project = new JSONObject(data);
-        jsonResponse = util.putRequest(reader.url, reader.authorization, id, project.toString());
+        jsonResponse = util.putRequest(READER.getEndPoint(), READER.getAuthorizationString(), id, project.toString());
     }
 
     @When("^I did a delete request with the id \"(.*?)\"")
-    public void iDidDeleteRequest(String id){
-        jsonResponse = util.deleteRequest(reader.url, reader.authorization, id);
+    public void iDidDeleteRequest(String id) throws Throwable {
+        jsonResponse = util.deleteRequest(READER.getEndPoint(), READER.getAuthorizationString(), id);
     }
 }
